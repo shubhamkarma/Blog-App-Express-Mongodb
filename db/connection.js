24044c08033b3a9ae059eb6b36d4eb874fdb7008
Shubham Karma <shubhamkarma2012@gmail.com>
2022-12-01 01:18:31 +0530

    first commit

---
+++ db/connection.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
+// const url = 'mongodb://localhost:27017';
+const url = 'mongodb+srv://shubhamkarma95:Maya2748@cluster0.sxzfirp.mongodb.net/blogsdb?retryWrites=true&w=majority';
+const client = new MongoClient(url);
+
+async function connectToDb() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to database server');
+//   const db = client.db(dbName);
+//   const collection = db.collection('blogs');
+
+  // the following code examples can be pasted here...
+
+  return client;
+}
+module.exports = connectToDb;
